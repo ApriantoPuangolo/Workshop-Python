97a91cd642772ab54335bf04ceb4a0ab96574d23
Aprianto A. Puangolo <113165502+ApriantoPuangolo@users.noreply.github.com>
2023-03-17 05:43:41 +0700

    Update bab3.py

--- Minggu-01/src/bab3.py
+++ Minggu-01/src/bab3.py
@@ -1,1 +1,32 @@
-
+>>> squares = [1, 4, 9, 16, 25]
+>>> squares
+[1, 4, 9, 16, 25]
+>>> squares[0]  # indexing returns the item
+1
+>>> squares[-1]
+25
+>>> squares[-3:]  # slicing returns a new list
+[9, 16, 25]
+>>> # Fibonacci series:
+>>> # the sum of two elements defines the next
+>>> a, b = 0, 1
+>>> while a < 10:
+...      print(a)
+...      a, b = b, a+b
+...
+0
+1
+1
+2
+3
+5
+8
+>>> i = 256*256
+>>> print('The value of i is', i)
+The value of i is 65536
+>>> a, b = 0, 1
+>>> while a < 1000:
+...     print(a, end=',')
+...     a, b = b, a+b
+...
+0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,>>>
