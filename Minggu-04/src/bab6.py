e7ea42a5b6e3a7159647e87f0e0dc3bb63d3ab0f
Aprianto A. Puangolo <113165502+ApriantoPuangolo@users.noreply.github.com>
2023-04-12 15:49:36 +0700

    Update bab6.py

--- Minggu-04/src/bab6.py
+++ Minggu-04/src/bab6.py
@@ -1,1 +1,111 @@
+  #Fibonacci numbers module
 
+    def fib(n):    # write Fibonacci series up to n
+        a, b = 0, 1
+        while a < n:
+            print(a, end=' ')
+            a, b = b, a+b
+        print()
+
+    def fib2(n):   # return Fibonacci series up to n
+        result = []
+        a, b = 0, 1
+        while a < n:
+            result.append(a)
+            a, b = b, a+b
+        return result
+
+      >>> import fibo
+      
+      >>>fibo.fib(1000)
+0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987
+>>>fibo.fib2(100)
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+>>>fibo.__name__
+'fibo'
+
+>>>fib = fibo.fib
+>>>fib(500)
+0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
+
+>>>from fibo import fib, fib2
+>>>fib(500)
+0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
+
+>>>from fibo import *
+>>>fib(500)
+0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
+
+python fibo.py <arguments>
+
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
+    
+$ python fibo.py 50
+0 1 1 2 3 5 8 13 21 34
+
+>>>import fibo
+>>>
+
+>>>import sys
+>>>sys.ps1
+'>>> '
+>>>sys.ps2
+'... '
+>>>sys.ps1 = 'C> '
+C> print('Yuck!')
+Yuck!
+C>
+
+>>>import fibo, sys
+>>>dir(fibo)
+['__name__', 'fib', 'fib2']
+>>>dir(sys)  
+['__breakpointhook__', '__displayhook__', '__doc__', '__excepthook__',
+ '__interactivehook__', '__loader__', '__name__', '__package__', '__spec__',
+ '__stderr__', '__stdin__', '__stdout__', '__unraisablehook__',
+ '_clear_type_cache', '_current_frames', '_debugmallocstats', '_framework',
+ '_getframe', '_git', '_home', '_xoptions', 'abiflags', 'addaudithook',
+ 'api_version', 'argv', 'audit', 'base_exec_prefix', 'base_prefix',
+ 'breakpointhook', 'builtin_module_names', 'byteorder', 'call_tracing',
+ 'callstats', 'copyright', 'displayhook', 'dont_write_bytecode', 'exc_info',
+ 'excepthook', 'exec_prefix', 'executable', 'exit', 'flags', 'float_info',
+ 'float_repr_style', 'get_asyncgen_hooks', 'get_coroutine_origin_tracking_depth',
+ 'getallocatedblocks', 'getdefaultencoding', 'getdlopenflags',
+ 'getfilesystemencodeerrors', 'getfilesystemencoding', 'getprofile',
+ 'getrecursionlimit', 'getrefcount', 'getsizeof', 'getswitchinterval',
+ 'gettrace', 'hash_info', 'hexversion', 'implementation', 'int_info',
+ 'intern', 'is_finalizing', 'last_traceback', 'last_type', 'last_value',
+ 'maxsize', 'maxunicode', 'meta_path', 'modules', 'path', 'path_hooks',
+ 'path_importer_cache', 'platform', 'prefix', 'ps1', 'ps2', 'pycache_prefix',
+ 
+ sound/                          Top-level package
+      __init__.py               Initialize the sound package
+      formats/                  Subpackage for file format conversions
+              __init__.py
+              wavread.py
+              wavwrite.py
+              aiffread.py
+              aiffwrite.py
+              auread.py
+              auwrite.py
+              ...
+      effects/                  Subpackage for sound effects
+              __init__.py
+              echo.py
+              surround.py
+              reverse.py
+              ...
+      filters/                  Subpackage for filters
+              __init__.py
+              equalizer.py
+              vocoder.py
+              karaoke.py
+              ...
+ 
+ import sound.effects.echo
+ 
+ sound.effects.echo.echofilter(input, output, delay=0.7, atten=4)
+ 
+ from sound.effects import echo
